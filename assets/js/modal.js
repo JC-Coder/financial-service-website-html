8d24ac4cdcfc39e7aabd831b321300d80bdbfe1a
JC-Coder <josephchimezie2003@gmail.com>
2023-01-06 09:54:51 +0100

    refactored validation function

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,7 +2,7 @@ let modal = document.querySelector("#modal");
 let closeModalBtn = modal.querySelector(".close");
 let modalToggle = document.querySelectorAll(".modalToggle");
 
-let modalMessage = modal.querySelector('.message').textContent;
+let modalMessage = modal.querySelector(".message").textContent;
 
 closeModalBtn.addEventListener("click", () => {
   modal.classList.toggle("closed");
@@ -12,23 +12,23 @@ modalToggle.forEach((item) => {
   item.addEventListener("click", () => {
     // validating inputs
     let inputFields = item.parentElement.querySelectorAll("input");
+    let modalMessageElement = modal.querySelector(".message");
 
-    for(let i = 0; i < inputFields.length; i++){
-        if (inputFields[i].value < 1) {
-            modal.querySelector('img').style.display = "none";
-            modal.querySelector('.message').textContent = "Kindly fill all fields to proceed";
-            modal.querySelector('.message').style.color = "red";
-            modal.classList.toggle("closed");
-            console.log(inputFields[i])
-            console.log(modal.innerHTML)
-            return;
-        }
+    for (let i = 0; i < inputFields.length; i++) {
+      if (inputFields[i].value < 1) {
+        modal.querySelector("img").style.display = "none";
+        modalMessageElement.textContent = "Kindly fill all fields to proceed";
+        modalMessageElement.style.color = "red";
+        modal.classList.toggle("closed");
+        console.log(inputFields[i]);
+        console.log(modal.innerHTML);
+        return;
+      }
     }
 
-
-    modal.querySelector('.message').textContent = modalMessage;
-    modal.querySelector('img').style.display = "inline-block";
-    modal.querySelector('.message').style.color = 'green';
+    modalMessageElement.textContent = modalMessage;
+    modal.querySelector("img").style.display = "inline-block";
+    modalMessageElement.style.color = "green";
     modal.classList.toggle("closed");
     item.parentElement.reset();
   });
